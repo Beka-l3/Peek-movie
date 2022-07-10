cfc4a9b571154f9533748bc88e17d827fda81356
Beka_l3 <b.talgat@innopolis.university>
2022-07-10 13:49:48 +0300

    Initial setup

---
+++ peek/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  peek
+//
+//  Created by Bekzhan Talgat on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
