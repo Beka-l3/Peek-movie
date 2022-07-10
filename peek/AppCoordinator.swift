dbb134ca9b7580087d4d4387b463b8a1475a7ee1
Beka_l3 <b.talgat@innopolis.university>
2022-07-10 15:48:31 +0300

    Add app coordinator

---
+++ peek/AppCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  AppCoordinator.swift
+//  peek
+//
+//  Created by Bekzhan Talgat on 10.07.2022.
+//
+
+import UIKit
+
+
+final class AppCoordinator {
+    
+    
+    init() {
+        
+        
+        
+    }
+    
+    
+}
